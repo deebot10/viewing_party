718c85d9802f1b0755ba44b78ec3891b0d9b2fa7
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-08-24 17:27:27 -0500

    Create users controller

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = user_params
+    user[:email] = user[:email].downcase
+    new_user = User.create(user)
+    flash[:success] = "Welcome, #{new_user.username}!"
+    redirect_to dashboard_path
+  end
+
+  def show
+    @user = User.new
+    @user.current_user = current_user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+end
