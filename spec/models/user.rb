dbb3c45a98be60c79b96a52a7c45a3442646164d
deebot10 <dee1jhill@gmail.com>
2021-08-23 19:04:06 -0600

    Create relationship/validation test for user

---
+++ spec/models/user.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  describe 'relationships' do
+    it {should have_many :friendships}
+    it {should have_many :friends, through: :friendships}
+    it {should have_many :viewing_parties}
+    it {should have_many :invitations}
+  end
+
+  describe 'validations' do
+    it {should validate_presence_of(:user_name) }
+    it {should validate_uniqueness_of(:user_name) }
+    it {should validate_presence_of(:email) }
+    it {should validate_uniqueness_of(:email) }
+    it {should validate_presence_of(:password) }
+  end
+end 
