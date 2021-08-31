a3352bcf9d9500bc1659126d137c78ec81248445
deebot10 <dee1jhill@gmail.com>
2021-08-30 18:39:41 -0600

    Add to party views

--- app/controllers/party_controller.rb
+++ app/controllers/party_controller.rb
@@ -2,7 +2,7 @@ class PartyController < ApplicationController
   def new
     @friends = current_user.friends
     @movie = MovieFacade.new.create_movie(params[:movie_id])
-    require 'pry'; binding.pry  
+    # require 'pry'; binding.pry  
   end
 
   def create
